a722d55c6c61542ccec551b2284a3c9206f67e15
Cody Hodges <hodges.cody00@gmail.com>
2022-04-04 09:30:05 -0500

    Beans

--- src/main/java/Album.java
+++ src/main/java/Album.java
@@ -1,9 +1,40 @@
 import java.io.Serializable;
+import java.util.Date;
 
 public class Album implements Serializable {
 
     public Album (){};
 
 
+    private long id;
+    private String artist;
+    private String name;
+    private Date releaseDate;
+    private long sales;
+    private String genre;
 
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
