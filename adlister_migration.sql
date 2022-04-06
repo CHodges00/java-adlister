4aedb98d624888626aa73dac819d807d87a35a1b
Cody Hodges <hodges.cody00@gmail.com>
2022-04-06 09:36:59 -0500

    JDBC

--- adlister_migration.sql
+++ adlister_migration.sql
@@ -1,15 +1,29 @@
 use adlister_db;
 
+
+DROP TABLE IF EXISTS ads;
+DROP TABLE IF EXISTS users;
+
+
+
 CREATE TABLE IF NOT EXISTS users(
     id  INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(25) DEFAULT 'NoName',
     email VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
 );
+ALTER TABLE users
+    ADD UNIQUE (username, email);
+
+
+
 
 CREATE TABLE IF NOT EXISTS ads(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(45),
-    description VARCHAR(500)
+    description VARCHAR(500),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
---
+++ adlister_seeder.sql
@@ -0,0 +1,13 @@
+use adlister_db;
+
+INSERT INTO  users(id, username, email, password)
+VALUES (0, 'iROWurBOAT', 'dgidhui7@yahoo.com', '1234djeyhd')
+;
+
+
+INSERT INTO ads (id, user_id, title, description)
+VALUES (0, 0, 'Couch', 'Buy now $599')
+;
+
+SELECT * FROM users;
+SELECT * FROM ads;
