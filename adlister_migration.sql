20108933aff684a40606d22e1d437d106c7ec46c
Cody Hodges <hodges.cody00@gmail.com>
2022-04-06 09:11:05 -0500

    JDBC

--- adlister_migration.sql
+++ adlister_migration.sql
@@ -1,3 +1,15 @@
-CREATE TABLE IF NOT EXISTS users{
+use adlister_db;
 
-    }
+CREATE TABLE IF NOT EXISTS users(
+    id  INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(25) DEFAULT 'NoName',
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ads(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title VARCHAR(45),
+    description VARCHAR(500)
+);
