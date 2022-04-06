0d4537fb09ed2fa5f1727f9a5623025308c38d28
Cody Hodges <hodges.cody00@gmail.com>
2022-04-06 10:35:41 -0500

    JDBC

--- src/main/java/AdsServer.java
+++ src/main/java/AdsServer.java
@@ -1,14 +1,48 @@
 
-import javax.servlet.ServletException;
+import java.sql.*;
+
+import com.mysql.cj.jdbc.Driver;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
+
+@WebServlet ("/ad")
+public class AdsServer extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false",
+                    "adUser",
+                    "4495"
+            );
+
+            Statement statement = connection.createStatement();
+
+            ResultSet rs = statement.executeQuery("SELECT * FROM ads;"); // Grabs info from database and bring back
+//            statement.execute() --- DELETE
+//              statement.executeUpdate() --- UPDATE
 
 
-public class AdsServer {
+
+            while (rs.next()){
+                quoteList.add(rs.getString("ads") + " | " + rs.getString("description"));
+
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+
+        }
 
 
+    }
+
+
+    public AdsServer() throws SQLException {
+
+    }
 }
